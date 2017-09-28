e613e4ec0badbc2a4fc5a4f98a5f09884a37bfe2
Seth Borne <sethborne@gmail.com>
2017-09-28 08:47:00 -0700

    freeCodeCamp - Object Oriented and Functional Programming - 11 - Concatenate Arrays with Concat - First Commit

---
+++ 01_Front_End_Development_Certification/F_Object_Oriented_and_Funcitonal_Programming/11_Concatenate_Arrays_with_Concat.js
@@ -0,0 +1,10 @@
+var oldArray = [ 1, 2, 3 ];
+var newArray = [];
+
+var concatMe = [ 4, 5, 6 ];
+
+// Under here
+
+newArray = oldArray.concat(concatMe);
+// newArray = concatMe.concat(oldArray);
+console.log(newArray);
